64884d5acece33c59add868789188be54ddc8ff7
AtlasChen <wjl1303121277@gmail.com>
2024-11-04 20:53:24 +0800

    export fallback

--- dn/flutter_bootstrap.js
+++ dn/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1774252262"
+    serviceWorkerVersion: "218469518"
   }
 });
