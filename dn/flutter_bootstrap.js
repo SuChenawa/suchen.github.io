4bd3c3e89903a4a65d9fcfac522faf2af4d935ab
AtlasChen <wjl1303121277@gmail.com>
2024-11-04 22:29:05 +0800

    .41update

--- dn/flutter_bootstrap.js
+++ dn/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3668922013"
+    serviceWorkerVersion: "2958719200"
   }
 });
