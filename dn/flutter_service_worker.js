4d947320948f2154aa450ddef85c71160eefe9bf
AtlasChen <admin@suchenawa.com>
2024-11-06 17:06:00 +0800

    dn:Score update

--- dn/flutter_service_worker.js
+++ dn/flutter_service_worker.js
@@ -23,14 +23,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "afe23f1d48b061e40d169a5684537754",
+"flutter_bootstrap.js": "6c5b2436e4db3102542c1abd537e5e66",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "e242471db7795a74c124684a5f5791bc",
 "/": "e242471db7795a74c124684a5f5791bc",
-"main.dart.js": "494bc3e2a367bad79ab942e533b55610",
+"main.dart.js": "4c2f07eff3aab36829c786d55deb2862",
 "manifest.json": "6bf51de730c820f28f2a273637f72b80",
 "version.json": "48c2989202a1de92f1f7cc54c71107ed"};
 // The application shell files that are downloaded before a service worker can
