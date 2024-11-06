248f17a265c591fd1b84d02cd759092e2b2719fb
AtlasChen <admin@suchenawa.com>
2024-11-06 11:29:26 +0800

    New DN UI

--- dn/flutter_bootstrap.js
+++ dn/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2958719200"
+    serviceWorkerVersion: "2468894088"
   }
 });
